7312a41d08262e32015a6bb60f1ce0ca96292998
tmstafford <tatumstafford1@gmail.com>
2020-12-07 16:51:04 -0600

    initial html routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const fs = require('fs');
 const path = require('path');
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
