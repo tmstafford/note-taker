a10b0347e1ecbab5db2aa2a4ef4e627725a9133b
tmstafford <tatumstafford1@gmail.com>
2020-12-11 16:26:36 -0600

    final changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-
+// uuid to generate unique id for each note 
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
@@ -11,7 +11,7 @@ router.get('/notes', (req, res) => {
 router.get('/notes/:id', (req, res) => {
     let allNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let idNote = (req.params.id).toString();
-
+    // filter through notes to get matching note id
     let result = allNotes.filter(val => val.id == idNote);
     res.json(result);
 });
@@ -38,11 +38,11 @@ router.post('/notes', (req, res) => {
 router.delete('/notes/:id', (req, res) => {
     let allNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let selectedId = (req.params.id).toString();
-
+    // filter through notes for id of note to be deleted
     allNotes = allNotes.filter(selectNote => {
         return selectNote.id != selectedId;
     });
-
+    // write file (db) with updated notes
     fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 2), err => {
         if (err) throw err;
         res.json(allNotes);
